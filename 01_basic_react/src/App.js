299f29b5c8aa26b0be8a9ce26da570ce95b4d214
AkshatBhatt11 <akshatbhatt12345@gmail.com>
2024-11-11 22:02:29 +0530

    Creating new Project with create react app and vite, with installing node modules in vite project

---
+++ 01_basic_react/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>This is React Project | Hello Akshat Bhatt | Learning ReactJS with Chai Aur Code</h1>
+  )
+}
+
+export default App;
---
+++ 01_vite_react/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  
+
+  return (
+  <h1>This is Vite Project | Hello Akshat Bhatt | Learning ReactJS with Chai Aur Code</h1>
+  )
+}
+
+export default App
