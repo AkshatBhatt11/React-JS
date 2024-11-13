ab0f1072cf2e6d4fb8763fa680375d38181c1c97
AkshatBhatt11 <akshatbhatt12345@gmail.com>
2024-11-13 13:47:02 +0530

    custom javascript in jsx file and many custom react render

--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -1,9 +1,11 @@
 import Chai from "./chai.jsx"
 
 function App() {
+  const userName = "akshat bhatt"
+
   return (<>
   <Chai/>
-  <h1>hello</h1>
+  <h1>hello {userName}</h1>
   </>);
 }
 
--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -1,9 +1,39 @@
 //import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+//import App from './App.jsx'
+import React from 'react'
+
+// function MyApp(){
+//   return(<>
+//   <h1>akshat bhatt</h1>
+//   </>)
+// }
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//        href: 'https://www.google.com/',
+//        target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+// const anotherElement = (
+//   <a href = 'https://www.google.com/' target='_blank'>Visit Google</a>
+// )
+
+const anotherUser = ' Google Link'
+const reactElement = React.createElement(
+  'a',{href: 'https://www.google.com/', target: '_blank'},'click for google', anotherUser
+)
+
 
 createRoot(document.getElementById('root')).render(
   //<StrictMode>
-    <App />
+  //<App/>
+    //<MyApp />,
+    //ReactElement
+    //anotherElement
+    reactElement
   //</StrictMode>,
 )
